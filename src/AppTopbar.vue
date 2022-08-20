8740789812b39a00af6d9c7bf57de864ee4a4070
Rustambek97 <r.baltayev9997@gmail.com>
2022-08-20 16:39:44 +0500

    first status

---
+++ src/AppTopbar.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="layout-topbar">
+		<router-link to="/" class="layout-topbar-logo">
+			<img alt="Logo" :src="topbarImage()" />
+			<span style="text-align: center;">Transport cars for tourists in Khiva</span>
+		</router-link>
+		<button class="p-link layout-menu-button layout-topbar-button" @click="onMenuToggle">
+			<i class="pi pi-bars"></i>
+		</button>
+
+		<button class="p-link layout-topbar-menu-button layout-topbar-button"
+			v-styleclass="{ selector: '@next', enterClass: 'hidden', enterActiveClass: 'scalein', 
+			leaveToClass: 'hidden', leaveActiveClass: 'fadeout', hideOnOutsideClick: true}">
+			<i class="pi pi-ellipsis-v"></i>
+		</button>
+		<ul class="layout-topbar-menu hidden lg:flex origin-top">
+			<li>
+				<button class="p-link layout-topbar-button">
+					<i class="pi pi-calendar"></i>
+					<span>Lang</span>
+				</button>
+			</li>
+			<li>
+				<button class="p-link layout-topbar-button">
+					<i class="pi pi-cog"></i>
+					<span>Today's weather</span>
+				</button>
+			</li>
+			<li>
+				<button class="p-link layout-topbar-button">
+					<i class="pi pi-user"></i>
+					<span>Exit</span>
+				</button>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+    methods: {
+        onMenuToggle(event) {
+            this.$emit('menu-toggle', event);
+        },
+		onTopbarMenuToggle(event) {
+            this.$emit('topbar-menu-toggle', event);
+        },
+		topbarImage() {
+			return this.$appState.darkTheme ? 'images/logo-white.svg' : 'images/logo-dark.svg';
+		}
+    },
+	computed: {
+		darkTheme() {
+			return this.$appState.darkTheme;
+		}
+	}
+}
+</script>
