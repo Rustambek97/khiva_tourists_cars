bbf1865dd2a9018ba31dcca53bf2dc6ea624fc9d
Rustambek97 <r.baltayev9997@gmail.com>
2022-08-30 01:54:43 +0500

    toggleData button

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -18,38 +18,17 @@
                                         ${{ product.data.price }}
                                     </h6>
                                 </div>
-                                <div>
-                                        <TabView style="text-align: justify;">
-                                            <TabPanel header="Header I">
-                                                <p class="line-height-3 m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></TabPanel>
-                                            <TabPanel header="Header II">
-                                                <p class="line-height-3 m-0">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque
-                                                    ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
-                                                    voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-                                                    Consectetur, adipisci velit, sed quia non numquam eius modi.</p>
-                                            </TabPanel>
-                                            <TabPanel header="Header III">
-                                                <p class="line-height-3 m-0">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
-                                                    et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.
-                                                    Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit
-                                                    quo minus.</p>
-                                            </TabPanel>
-                                        </TabView>
-                                   
-                                </div>
+                                
                                 <div>
                                     <div class="col-12">
                                         <Button type="button" label="Maps" @click="toggle" class="p-button-success"/>
-                                        <OverlayPanel ref="op" appendTo="body" :showCloseIcon="true">
+                                        <OverlayPanel ref="op" appendTo="body" :showCloseIcon="true" style="width: 25rem">
                                             <img src="images/nature/nature9.jpg" alt="Nature 9" />
                                         </OverlayPanel>
                                     </div>
                                     <div class="col-12">
                                         <Button type="button" label="Price for Cars" @click="toggleDataTable" class="p-button-success"/>
-                                        <OverlayPanel ref="op2" appendTo="body" :showCloseIcon="true" id="overlay_panel" style="width: 450px">
+                                        <OverlayPanel ref="op2" appendTo="body" :showCloseIcon="true" id="overlay_panel" style="width: 25rem">
                                             <DataTable :value="products" v-model:selection="selectedProduct" selectionMode="single" :paginator="true" :rows="5" @row-select="onProductSelect" responsiveLayout="scroll">
                                                 <Column field="name" header="Name" :sortable="true" headerStyle="min-width:10rem;"></Column>
                                                 <Column header="Image" headerStyle="min-width:10rem;">
@@ -66,9 +45,9 @@
                                         </OverlayPanel>
                                     </div>
                                     <div class="col-12">
-                                        <div class="card">
-                                            <Button type="button" label="Maps" @click="toggle" class="p-button-success"/>
-                                            <OverlayPanel ref="op" appendTo="body" :showCloseIcon="true">
+                                   
+                                            <Button type="button" label="Galleria" @click="toggleGalleria" class="p-button-success"/>
+                                            <OverlayPanel ref="op3" appendTo="body" :showCloseIcon="true">
                                                 <Galleria :value="images" :responsiveOptions="galleriaResponsiveOptions" :numVisible="7" :circular="true" containerStyle="max-width: 300px; margin: auto">
                                                     <template #item="slotProps">
                                                         <img :src="slotProps.item.itemImageSrc" :alt="slotProps.item.alt" style="width: 100%; display: block" />
@@ -78,14 +57,39 @@
                                                     </template>
                                                 </Galleria>
                                             </OverlayPanel>
-                                            
-                                        </div>
                                     </div>
+                                    <div class="col-12">
+                                        <Button type="button" label="Data" @click="toggleData" class="p-button-success"/>
+                                        <OverlayPanel style="width: 25rem" ref="op4" appendTo="body" :showCloseIcon="true">
+                                            <TabView style="text-align: justify;">
+                                                <TabPanel header="Header I">
+                                                    <p class="line-height-3 m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></TabPanel>
+                                                <TabPanel header="Header II">
+                                                    <p class="line-height-3 m-0">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque
+                                                        ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+                                                        voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+                                                        Consectetur, adipisci velit, sed quia non numquam eius modi.</p>
+                                                </TabPanel>
+                                                <TabPanel header="Header III">
+                                                    <p class="line-height-3 m-0">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
+                                                        et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.
+                                                        Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit
+                                                        quo minus.</p>
+                                                </TabPanel>
+                                            </TabView>
+                                        </OverlayPanel>
+                                </div>
                                 </div>
                             </div>
                         </div>
+                        
                     </template>
+                    
                 </Carousel>
+                
             </div>
         </div>
     </div>
@@ -102,17 +106,17 @@ export default {
             images: [],
             carouselResponsiveOptions: [
                 {
-                    breakpoint: "1500px",
+                    breakpoint: "1024px", //1500
                     numVisible: 3,
                     numScroll: 3,
                 },
                 {
-                    breakpoint: "1392px",
+                    breakpoint: "768px", //1392
                     numVisible: 2,
                     numScroll: 2,
                 },
                 {
-                    breakpoint: "824px",
+                    breakpoint: "560px", //824
                     numVisible: 1,
                     numScroll: 1,
                 },
@@ -149,6 +153,12 @@ export default {
 			},
 			toggleDataTable(event) {
 				this.$refs.op2.toggle(event);
+			},
+            toggleGalleria(event) {
+				this.$refs.op3.toggle(event);
+			},
+            toggleData(event) {
+				this.$refs.op4.toggle(event);
 			},
 			formatCurrency(value) {
 				return value.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
